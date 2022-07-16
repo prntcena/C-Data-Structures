c5d942f20dd50effad4599bc4e7fb90ea1168a5c
Praneet Singh <gk68118@gmail.com>
2022-07-16 12:53:26 +0530

    Update buySellStock.cpp

--- DSASheets/Fraz/buySellStock.cpp
+++ DSASheets/Fraz/buySellStock.cpp
@@ -15,3 +15,23 @@ public:
         
     }
 };
+
+
+// part 2 solution 
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        for(int i=0;i<prices.size()-1;i++)
+        {
+            if(prices[i+1]>prices[i])
+            {
+                profit += prices[i+1] - prices[i];
+            }
+        }
+        return profit;
+    }
+};
+
+
