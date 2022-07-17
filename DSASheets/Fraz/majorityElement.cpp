6198a2db1e9cf3cbe307f4ad7e9bb989ade546d8
Praneet Singh <gk68118@gmail.com>
2022-07-17 11:25:13 +0530

    Update majorityElement.cpp

--- DSASheets/Fraz/majorityElement.cpp
+++ DSASheets/Fraz/majorityElement.cpp
@@ -10,3 +10,21 @@ public:
 
 // chori kiya hua hai.
 //Note:- what is BMV algorithm
+
+
+int majorityElement(vector<int>& nums) {
+        int ans = 0;
+        int count = 1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[ans])
+                count++;
+            else
+                count--;
+                
+            if(count == 0){
+                count++;
+                ans = i;
+            }
+        }
+        return nums[ans];
+    }
